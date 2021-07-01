db26a0094df3b0c8c33f5a303b10a488dfa2b254
Matt Kaiser <matt@kaiserengineering.io>
2021-07-01 01:03:05 -0400

    Initial commit of code, not fully functional yet

---
+++ inc/lib_vehicle_data.h
@@ -0,0 +1,55 @@
+#ifndef LIB_VEHICLE_DATA_H
+#define LIB_VEHICLE_DATA_H
+
+#include "lib_pid.h"
+
+#define LIB_VEHICLE_MAX_PARAMS 5
+#define LIB_VEHICLE_MAX_DATA LIB_VEHICLE_MAX_PARAMS * 2
+#define LIB_VEHICLE_DATA_BUFFER_SIZE 10
+
+typedef enum _vehicle_data_status {
+    VEHICLE_PARAM_UNSUPPORTED,
+    VEHICLE_DATA_OK,
+    VEHICLE_MAX_PARAMS,
+} VEHICLE_DATA_STATUS, *PTR_VEHICLE_DATA_STATUS;
+
+typedef struct _vehicle_data_equation {
+    uint8_t equation;
+        #define VEHICLE_DATA_EQ_NOT_DEFINED     0
+        #define VEHICLE_DATA_EQ_VAL1_MINUS_VAL2 1
+    uint8_t val1;
+    uint8_t val2;
+} VEHICLE_DATA_EQUATION, *PTR_VEHICLE_DATA_EQUATION;
+
+typedef struct _vehicle_data_manager {
+
+    PTR_PID_DATA stream[LIB_VEHICLE_MAX_PARAMS];
+
+    PTR_PID_DATA data[LIB_VEHICLE_MAX_DATA];
+
+    VEHICLE_DATA_EQUATION formula[LIB_VEHICLE_MAX_PARAMS];
+
+    uint8_t data_status[LIB_VEHICLE_MAX_DATA];
+        #define VEHICLE_DATA_STATUS_READY  0
+        #define VEHICLE_DATA_STATUS_REMOVE 1
+        #define VEHICLE_DATA_STATUS_ADD    2
+
+    clear_pid_request clear_pid;
+
+    request_pid_data req_pid;
+
+    uint8_t num_data;
+
+    uint8_t num_pids;
+
+} VEHICLE_DATA_MANAGER, *PTR_VEHICLE_DATA_MANAGER;
+
+void Vehicle_Init( PTR_VEHICLE_DATA_MANAGER dev );
+
+VEHICLE_DATA_STATUS Vehicle_add_parameter( PTR_VEHICLE_DATA_MANAGER dev, PTR_PID_DATA pid );
+
+void Vehicle_service( PTR_VEHICLE_DATA_MANAGER dev );
+
+void Vehicle_tick( void );
+
+#endif // LIB_VEHICLE_DATA_H
---
+++ src/lib_vehicle_data.c
@@ -0,0 +1,149 @@
+#include "lib_vehicle_data.h"
+
+uint8_t new_req = 0;
+
+uint32_t vehicle_tick = 0;
+
+void Vehicle_Init( PTR_VEHICLE_DATA_MANAGER dev )
+{
+    dev->num_pids = 0;
+    dev->num_data = 0;
+
+    for( uint8_t i = 0; i < LIB_VEHICLE_MAX_DATA; i++)
+        lib_pid_clear_PID( dev->data[i] );
+
+    for( uint8_t i = 0; i < LIB_VEHICLE_MAX_PARAMS; i++)
+        dev->formula[i].equation = VEHICLE_DATA_EQ_NOT_DEFINED;
+}
+
+VEHICLE_DATA_STATUS Vehicle_add_parameter( PTR_VEHICLE_DATA_MANAGER dev, PTR_PID_DATA pid )
+{
+    switch( pid->mode )
+    {
+        case CALC1:
+            switch( pid->pid )
+            {
+                case CALC1_TURBOCHARGER_COMPRESSOR_INLET_PRESSURE:
+
+                    /* Set the flag to later request the needed data */
+                    new_req = 1;
+
+                    /* Verify another PID can be added */
+                    if( dev->num_pids + 1 > LIB_VEHICLE_MAX_PARAMS )
+                        return VEHICLE_MAX_PARAMS;
+
+                    /* Save the PID pointer */
+                    dev->stream[dev->num_pids++] = pid;
+
+                    return VEHICLE_DATA_OK;
+
+                default:
+                    return VEHICLE_PARAM_UNSUPPORTED;
+            }
+            break;
+
+        default:
+            return VEHICLE_PARAM_UNSUPPORTED;
+    }
+}
+
+VEHICLE_DATA_STATUS Vehicle_remove_PID_request( PTR_VEHICLE_DATA_MANAGER dev, PTR_PID_DATA pid )
+{
+    /* Set the request to sync PIDs */
+    new_req = 1;
+
+    /* Cycle through all the PIDs to find which one must be removed */
+    for( uint8_t index = 0; index < dev->num_pids; index++ )
+    {
+        /* If found, pop that pointer reference */
+        if( dev->stream[index] == pid )
+        {
+            if( dev->num_pids > 1 )
+            {
+                for( uint8_t i = index; i < dev->num_pids; i++ ) {
+                    dev->stream[i] = dev->stream[i + 1];
+                    dev->stream[i + 1] = NULL;
+                }
+            }
+
+            /* Remove the PID */
+            if( dev->num_pids > 0 )
+                dev->num_pids--;
+
+            /* Return a success */
+            return VEHICLE_DATA_OK;
+        }
+    }
+
+    /* Return a success */
+    return VEHICLE_DATA_OK;
+}
+
+void Vehicle_service( PTR_VEHICLE_DATA_MANAGER dev )
+{
+    if( new_req )
+    {
+        PID_DATA req;
+        for( uint8_t i = 0; i < dev->num_pids; i++)
+        {
+            switch( dev->stream[i]->mode )
+            {
+                case CALC1:
+                    switch( dev->stream[i]->pid )
+                    {
+                        case CALC1_TURBOCHARGER_COMPRESSOR_INLET_PRESSURE:
+                            req.mode     = MODE1;
+                            req.pid      = MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE;
+                            req.pid_unit = MODE1_INTAKE_MANIFOLD_ABSOLUTE_PRESSURE_UNITS;
+
+                            /* Save the MAP data index */
+                            dev->formula[i].val1 = dev->num_data;
+
+                            /* Add the PID request */
+                            dev->data[dev->num_data++] = dev->req_pid( &req );
+
+                            req.mode     = MODE1;
+                            req.pid      = MODE1_ABSOLUTE_BAROMETRIC_PRESSURE;
+                            req.pid_unit = MODE1_ABSOLUTE_BAROMETRIC_PRESSURE_UNITS;
+
+                            /* Save the Baro data index */
+                            dev->formula[i].val2 = dev->num_data;
+
+                            /* Add the PID request */
+                            dev->data[dev->num_data++] = dev->req_pid( &req );
+
+                            /* Boost = MAP - Baro */
+                            dev->formula[i].equation = VEHICLE_DATA_EQ_VAL1_MINUS_VAL2;
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        new_req = 0;
+    }
+
+    for( uint8_t i = 0; i < dev->num_pids; i++)
+    {
+        switch( dev->formula[i].equation )
+        {
+            case VEHICLE_DATA_EQ_VAL1_MINUS_VAL2:
+                dev->stream[i]->pid_value = dev->data[dev->formula[i].val1]->pid_value - dev->data[dev->formula[i].val2]->pid_value;
+                if( dev->data[dev->formula[i].val1]->timestamp > 0)
+                dev->stream[i]->timestamp = vehicle_tick;
+                break;
+
+            case VEHICLE_DATA_EQ_NOT_DEFINED:
+            default:
+                break;
+        }
+    }
+}
+
+void Vehicle_tick( void )
+{
+    vehicle_tick++;
+}
